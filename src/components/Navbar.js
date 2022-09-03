dffe3aed1ed074739d2789e570d69f0b2b057cd0
Minh Nguyen <gin411@vagelos-ve702-1649.apng.wlan.private.upenn.edu>
2022-09-03 17:03:32 -0400

    Add navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import Logo from "../images/Rectangle1.svg";
+import joinIcon from "../images/joinIcon2.png";
+import createIcon2 from "../images/addIcon.png";
+export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+  console.log(isOpen);
+  return (
+    <div className="bg-orange-600">
+      <nav className="flex items-center  flex-wrap cursor-pointer">
+        <div className="justify-between flex w-full items-center">
+          <div className=" cursor-pointer flex items-center">
+            <img src={Logo} alt="logo" className="h-9 w-9 inline"></img>
+            <h1 className="font-serif hover:text-neutral-50	duration-500 ml-1">
+              {" "}
+              DISCOTHEQUE{" "}
+            </h1>
+          </div>
+
+          <div className="hidden md:flex justify-around w-full">
+            <div className="flex">
+              <h1 className="font-serif hover:text-neutral-50	duration-500">
+                Join A Session
+              </h1>
+              <img
+                src={joinIcon}
+                alt="joinIcon"
+                className="h-6 w-6 inline"
+              ></img>
+            </div>
+            <div className="flex">
+              <h1 className="font-serif hover:text-neutral-50	duration-500">
+                Create New Session
+              </h1>
+              <img
+                src={createIcon2}
+                alt="createIcon"
+                className="h-6 w-6 inline"
+              ></img>
+            </div>
+          </div>
+          {/* Mobile Navbar Starting */}
+          <div className="flex md:hidden">
+            <i
+              className="fas fa-bars mr-2"
+              onClick={() => setIsOpen(!isOpen)}
+            >Menu</i>
+            {isOpen ? (
+              <div className="fixed inset-0 w-full h-full bg-orange-600">
+                <i
+                  className="fas fa-xmark mr-2 text-2xl mt-4"
+                  onClick={() => setIsOpen(!isOpen)}
+                >Close Menu</i>
+                <div className="flex flex-col h-[70%] justify-center text-xl">
+                  <div className="flex justify-center">
+                    <h1 className="font-serif hover:text-neutral-50	duration-500">
+                      Join A Session
+                    </h1>
+                    <img
+                      src={joinIcon}
+                      alt="joinIcon"
+                      className="h-6 w-6 inline"
+                    ></img>
+                  </div>
+                  <div className="flex justify-center">
+                    <h1 className="font-serif hover:text-neutral-50	duration-500">
+                      Create New Session
+                    </h1>
+                    <img
+                      src={createIcon2}
+                      alt="createIcon"
+                      className="h-6 w-6 inline"
+                    ></img>
+                  </div>
+                </div>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Josefin Slab'],
+      'serif': ['Josefin Slab'],
+      'display': ['Josefin Slab'],
+      'body': ['Josefin Slab'],
+
+    },
   },
   plugins: [],
 }
+
